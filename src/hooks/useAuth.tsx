399530cd5369f8fc5882fe04024837dc59485584
imoguz <imoguz0510@gmail.com>
2024-01-20 00:28:03 +0300

    authHook fixed

--- src/hooks/useAuth.tsx
+++ src/hooks/useAuth.tsx
@@ -3,31 +3,38 @@ import { useAuthContext } from "../context/authContext";
 import { toastifyError, toastifySuccess } from "../helpers/toastify";
 const useAuth = () => {
   const { setUserData } = useAuthContext();
+  const baseURL = process.env.REACT_APP_BASE_URL;
 
   const signup = async (formData: ISignupInitialValues) => {
     try {
-      await axios.post("http://localhost:8000/yami/users", formData);
-      signin({ email: formData.email, password: formData.password });
+      await axios.post(baseURL + "/users", formData);
+      await signin(
+        { email: formData.email, password: formData.password },
+        true
+      );
     } catch (error) {
-      console.log(error);
-      // hata durum mesajlarını spesific yazdır.
-      toastifyError("An unexpected error occurred. Please try again later.");
+      toastifyError(
+        "Registration unsuccessful. Please check your information or connection and try again."
+      );
     }
   };
 
-  const signin = async (formData: ISigninInitialValues) => {
+  const signin = async (
+    formData: ISigninInitialValues,
+    signup: boolean = false
+  ) => {
     try {
-      const { data } = await axios.post(
-        "http://localhost:8000/yami/auth/login",
-        formData
-      );
+      const { data } = await axios.post(baseURL + "/auth/login", formData);
       sessionStorage.setItem("userData", JSON.stringify(data));
       setUserData(data);
-      toastifySuccess("You have successfully logged in.");
+      toastifySuccess(
+        `You have successfully ${signup && "signup and"} sign in.`
+      );
     } catch (error) {
       console.log(error);
-      // hata durum mesajlarını spesific yazdır.
-      toastifyError("An unexpected error occurred. Please try again later.");
+      toastifyError(
+        "Sign in unsuccessful. Please check your information or connection and try again."
+      );
     }
   };
 
